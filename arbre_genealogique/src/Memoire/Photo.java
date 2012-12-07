da318703d5e0ba1e696d047ae7b7941fdbd6c6ff
Cindy Perat <cindy.perat@live.fr>
2012-12-07 09:33:59 +0100

    .

--- arbre_genealogique/src/Memoire/Photo.java
+++ arbre_genealogique/src/Memoire/Photo.java
@@ -2,21 +2,34 @@ package Memoire;
 
 
 /**
- * Cette classe 
+ * Cette classe correspond aux photos attribuées aux personnes de l'arbre généalogique.
+ * Une photo est désignée par son nom.
  *
  */
 public class Photo {
     private String nomImage;
     
+    /**
+     * 
+     * @param nomImage Nom de la photo
+     */
     public Photo(String nomImage){
-	this.nomImage = nomImage;
+    	this.nomImage = nomImage;
     }
 
+    /**
+     * Récupère le nom de la photo d'une personne
+     * @return Nom de la photo
+     */
     public String getNomImage() {
-	return nomImage;
+    	return nomImage;
     }
 
+    /**
+     * Modifie le nom de la photo
+     * @param nomImage Nom de la photo
+     */
     public void setNomImage(String nomImage) {
-	this.nomImage = nomImage;
+    	this.nomImage = nomImage;
     }
 }
